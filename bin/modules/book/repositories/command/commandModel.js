5153ee26c44743251128aa904574438ba8d03fed
edobintang <rokatendaedo@gmail.com>
2023-12-08 19:16:34 +0700

    add date validation type on schedule field request

--- bin/modules/book/repositories/command/commandModel.js
+++ bin/modules/book/repositories/command/commandModel.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 const postBookSchedule = joi.object({
     username: joi.string().required(),
     book_key: joi.string().required(),
-    schedule: joi.string().required()
+    schedule: joi.date().iso().required()
 });
 
 module.exports = {
